716b219c85cae0ca47b09c0afb9ec760dcb167f2
thiliniprr <49749994+thiliniprr@users.noreply.github.com>
2025-08-25 12:57:07 +0300

    Update chemprot_relation_classification.py

--- chemprot_relation_classification.py
+++ chemprot_relation_classification.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Aug 24 17:00:43 2025
 
 @author: pereran
 """
@@ -145,5 +144,6 @@ print("CUDA available:", torch.cuda.is_available())
 print("CUDA device count:", torch.cuda.device_count())
 if torch.cuda.is_available():
     print("CUDA device name:", torch.cuda.get_device_name(0))
+
 
 
